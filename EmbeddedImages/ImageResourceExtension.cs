7e0319b67c2df3e1e260082bb36e405773b25d11
Elvis Geovanny <elvis.batzibal@hotmail.es>
2019-12-31 16:52:57 -0600

    Configured ImageResource
    Configured ImageResource in ImageResourceExtension
    and Image Xaml

--- EmbeddedImages/ImageResourceExtension.cs
+++ EmbeddedImages/ImageResourceExtension.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using System.Reflection;
 
 namespace EmbeddedImages
 {
-    public class ImageResourceExtension
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
     {
+
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            //System.Reflection GetTypeInfo().Assembly
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
     }
 }
